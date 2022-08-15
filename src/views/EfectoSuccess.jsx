79119063affff48501037db3dc1846c0c4173b90
JosejCruz <jcruz@medicaresoftsrl.com>
2022-08-15 17:36:57 -0400

    Prueba v1.05
    Primera pruebas para registro de pacientes

--- src/views/EfectoSuccess.jsx
+++ src/views/EfectoSuccess.jsx
@@ -3,8 +3,8 @@ import React from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-export default function EfectoSucces() {
-  const notify = () => toast.success('Registro exitoso', {
+export default function EfectoSuccess() {
+  const Notify = () => toast.success('Registro exitoso', {
     position: "bottom-center",
     autoClose: 5000,
     hideProgressBar: true,
@@ -26,11 +26,11 @@ export default function EfectoSucces() {
         pauseOnFocusLoss
         draggable
         pauseOnHover
-        />
-        
+      />
     </div>
   );
 }
 
 
+//<button onClick={notify}>Notificación</button>
 //para las notificaciones https://www.npmjs.com/package/react-toastify
--- src/views/Registro.jsx
+++ src/views/Registro.jsx
@@ -1,10 +1,13 @@
 //import "../css/Registro.css"
 //import {IonIcon} from "react-ion-icon";
-
+import {Pacientejson} from '../interfaces/Paciente'
+import {Medicojson} from '../interfaces/Medico'
+import {Segurojson} from '../interfaces/Seguro'
+import {Ingresojson} from '../interfaces/Ingreso'
 import { useState } from "react";
 import PantallaEspera from "./PantallaEspera";
 import EfectoEspera from "./EfectoEspera";
-import EfectoSucces from "./EfectoSuccess";
+import EfectoSuccess from "./EfectoSuccess";
 
 export default function RegitroPaciente(){
     const [Estado, SetEstado] = useState(false);
@@ -29,6 +32,59 @@ export default function RegitroPaciente(){
         */
     }
 
+    //Estados y funciones UseState
+    //Paciente
+    const [Paciente, setPaciente] = useState(Pacientejson)
+    const [ID, setID] = useState('')
+    const [nombre, setNombre] = useState('')
+    const [telefono, setTelefono] = useState(0)
+    const [correo, setCorreo] = useState('')
+    const [edad, setEdad] = useState('')
+
+    //usestate Medico
+    const [Medico, setMedico] = useState(Medicojson)
+    const [nombremedico, setNombremedico] = useState('')
+    //usestate Seguro
+    const [Seguro, setSeguro] = useState(Segurojson)
+    const [nombreSeguro, setNombreSeguro] = useState('')
+    const [cobertura, setCobertura] = useState(0)
+
+    //useState Ingreso
+    const [ingreso, setIngreso] = useState(Ingresojson)
+
+    function GuardarPaciente(){
+        const newPaciente = {
+            ID,
+            nombre,
+            telefono,
+            correo,
+            edad: parseInt(edad)
+        }
+        const newMedico = {
+            nombre: nombremedico
+        }
+        const newSeguro = {
+            nombre: nombreSeguro,
+            cobertura: parseInt(cobertura)
+        }
+        
+        setPaciente(newPaciente)
+        setMedico(newMedico)
+        setSeguro(newSeguro)
+        const newIngreso = {
+          Paciente: Paciente,
+          Medico: Medico,
+          Seguro: Seguro
+        };
+        setIngreso(newIngreso)
+
+        console.log(Paciente)
+        console.log(Medico)
+        console.log(Seguro)
+        console.log(ingreso)
+    }
+
+
     return (
         <>
         <div className="container mt-3">
@@ -44,7 +100,7 @@ export default function RegitroPaciente(){
             </div>
             <div className="row">
                 <div className="form-floating col-sm-12 col-md-4 col-lg-4">
-                    <input type="text" className="form-control mb-2" placeholder="C.I." required/>
+                    <input type="text" className="form-control mb-2" placeholder="C.I." onChange={(e) => {setID(e.target.value)}} required/>
                     <label htmlFor="floatingInput">CI</label>
                 </div>
                 <div className="form-floating col-sm-12 col-md-4 col-lg-4">
@@ -52,19 +108,11 @@ export default function RegitroPaciente(){
                     <label htmlFor="floatingInput">Fecha</label>
                 </div>
                 <div className="col-sm-12 col-md-4 col-lg-4 mb-2 d-grid gap-2">
-                    <button className="btn btn-success" >Buscar <ion-icon name="search"></ion-icon></button>
+                    <button className="btn btn-success" onClick={GuardarPaciente}>Buscar <ion-icon name="search"></ion-icon></button>
                 </div>
-                <div className="form-floating col-sm-12 col-md-4 col-lg-4">
-                    <input type="text" className="form-control mb-2" placeholder="Nombre" required/>
-                    <label htmlFor="floatingInput">Nombre</label>
-                </div>
-                <div className="form-floating col-sm-12 col-md-4 col-lg-4">
-                    <input type="text" className="form-control mb-2" placeholder="Apellido Paterno" required/>
-                    <label htmlFor="floatingInput">Apellido Paterno</label>
-                </div>
-                <div className="form-floating col-sm-12 col-md-4 col-lg-4">
-                    <input type="text" className="form-control mb-2" placeholder="Apellido Materno" required/>
-                    <label htmlFor="floatingInput">Apellido Materno</label>
+                <div className="form-floating col-sm-12 col-md-12 col-lg-12">
+                    <input type="text" className="form-control mb-2" placeholder="Nombre Completo" onChange={(e) => {setNombre(e.target.value)}} required/>
+                    <label htmlFor="floatingInput">Nombre Completo</label>
                 </div>
                 <div className="form-floating col-sm-12 col-md-4 col-lg-4">
                     <select className="form-select mb-2">
@@ -75,35 +123,39 @@ export default function RegitroPaciente(){
                     <label htmlFor="floatingInput">Sexo</label>
                 </div>
                 <div className="form-floating col-sm-12 col-md-4 col-lg-4">
-                    <input type="text" className="form-control mb-2" placeholder="Dirección" required/>
-                    <label htmlFor="floatingInput">Dirección</label>
+                    <input type="text" className="form-control mb-2" placeholder="Persona Responsable" onChange={(e) => {parseInt(setEdad(e.target.value))}} required/>
+                    <label htmlFor="floatingInput">Edad</label>
                 </div>
                 <div className="form-floating col-sm-12 col-md-4 col-lg-4">
-                    <input type="text" className="form-control mb-2" placeholder="Teléfono" required/>
+                    <input type="text" className="form-control mb-2" placeholder="Teléfono" onChange={(e) => {setTelefono(e.target.value)}} required/>
                     <label htmlFor="floatingInput">Teléfono</label>
                 </div>
                 <div className="form-floating col-sm-12 col-md-4 col-lg-4">
-                    <input type="email" className="form-control mb-2" placeholder="Correo" required/>
+                    <input type="email" className="form-control mb-2" placeholder="Correo" onChange={(e) => {setCorreo(e.target.value)}} required/>
                     <label htmlFor="floatingInput">Correo</label>
                 </div>
-                <div className="form-floating col-sm-12 col-md-8 col-lg-8">
-                    <input type="text" className="form-control mb-2" placeholder="Persona Responsable" required/>
-                    <label htmlFor="floatingInput">Persona Responsable</label>
+                <div className="form-floating col-sm-12 col-md-4 col-lg-4">
+                    <input type="text" className="form-control mb-2" placeholder="Nombre Medico" onChange={(e) => {setNombremedico(e.target.value)}} required/>
+                    <label htmlFor="floatingInput">Medico</label>
                 </div>
-                <div className="mb-3">
-                    <label htmlFor="exampleFormControlTextarea1" className="form-label">Nota</label>
-                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Nota"></textarea>
+                <div className="form-floating col-sm-12 col-md-4 col-lg-4">
+                    <input type="text" className="form-control mb-2" placeholder="Seguro" onChange={(e) => {setNombreSeguro(e.target.value)}} required/>
+                    <label htmlFor="floatingInput">Seguro</label>
                 </div>
-                <div className="col-sm-12 col-md-4 col-lg-3 d-grid gap-2">
+                <div className="form-floating col-sm-12 col-md-4 col-lg-4">
+                    <input type="text" className="form-control mb-2" placeholder="Cobertura" onChange={(e) => {setCobertura(e.target.value)}} required/>
+                    <label htmlFor="floatingInput">Cobertura</label>
+                </div>
+                <div className="col-sm-12 col-md-4 col-lg-4 d-grid gap-2">
                     <button className="btn btn-primary mb-2" onClick={CambiarEstado}><ion-icon name="save-outline"></ion-icon> Guardar</button>
                 </div>
-                <div className="col-sm-12 col-md-4 col-lg-3 d-grid gap-2">
+                <div className="col-sm-12 col-md-4 col-lg-4 d-grid gap-2">
                     <button className="btn btn-danger  mb-2" onClick={CambiarEstado2}><ion-icon name="ban-outline"></ion-icon> Cancelar</button>
                 </div>
             </div>
         </div>
         {Estado&&<EfectoEspera/>}
-        {Estado2&&<EfectoSucces/>}
+        {Estado2&&<EfectoSuccess/>}
         </>
     ); 
 }
